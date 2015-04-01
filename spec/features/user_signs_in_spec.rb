4f08d89b3a9f64f863e5360d343dfb9364b0fffe
Richard Davis <rdavis.bacs@gmail.com>
2015-04-01 14:33:08 -0400

    spec for signing in

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+feature "user signs in" do
+  scenario "with valid credentials" do
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    click_link "Sign In"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign In"
+
+    expect(page).to have_content("Thank you for signing in.")
+    expect(page).to_not have_link("Sign Up")
+  end
+end
